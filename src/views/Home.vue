3ae2bb619693244a7214bcb6e6f95342ca9b288c
Peempat Pinsang <peempat.pinsang@mail.kmutt.ac.th>
2021-04-17 18:29:39 +0700

    fix images in HOME page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,23 @@
 <template>
   <div class="home">
     <div class="container">
-      <img src="@/assets/images/slide1.png" width="900" height="900">
-      <img src="@/assets/images/slide2.png">
-      <img src="@/assets/images/slide3.png" width="900" height="900">
-      <slide></slide>
+      <img src="@/assets/images/slide1.png" class="slidelogo">
+      <img src="@/assets/images/slide2.png" class="slidelogo">
+      <img src="@/assets/images/slide3.png" class="slidelogo">
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Slide from "@/components/UI/Slide.vue"
 export default {
   name: "Home",
   components: {
-    Slide
   },
   data(){
     return{
-      images:[
-        '@/assets/images/slide1.png',
-        '@/assets/images/slide2.png',
-        '@/assets/images/slide3.png'
-      ]
+      
     }
   }
 };
 </script>
-<style scoped>
-  .slideContainer{
-    width:960px;
-    margin:0 auto;
-    text-align: center;
-  }
-</style>
